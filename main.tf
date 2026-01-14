86b6b8d5adae615d515f0221427093c37857824e
Amrendra <azlab16_sbe@lernlyticaprojectgmail.onmicrosoft.com>
2026-01-14 13:19:26 +0530

    Terraform Azure Jenkins lab - initial commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "Stark-vision-ai-lab-rg"
+    storage_account_name = "starkstroage"
+    container_name       = "starkc"
+    key                  = "jenkins-demo.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "JenkinsTerraformRG"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "JenkinsVNet"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
